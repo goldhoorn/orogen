b13f7402b2294335f5d6b69bea37d9dc5728352a
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-04-30 15:04:35 +0200

    First implementation of class_loader suppoer for dynamic_task deployment

--- lib/orogen/templates/tasks/DeployerComponent.cpp
+++ lib/orogen/templates/tasks/DeployerComponent.cpp
@@ -1,5 +1,9 @@
 #include <rtt/Component.hpp>
 
+#include <orogen/deployer.hpp>
+
+#include <class_loader/class_loader.h>
+
 <% deployable_tasks = project.self_tasks.find_all { |t| !t.abstract? } %>
 <% deployable_tasks.each do |task| %>
 #include <<%= task.header_file %>>
@@ -12,6 +16,7 @@ ORO_LIST_COMPONENT_TYPE( <%= task.name %> );
 
 namespace orogen
 {
+
 <% deployable_tasks.each do |task| %>
     RTT::TaskContext* create_<%= task.name.gsub(/[^\w]/, '_') %>(std::string const& instance_name)
     {
@@ -19,3 +24,21 @@ namespace orogen
     }
 <% end %>
 }
+
+<% deployable_tasks.each do |task| %>
+<% task.full_namespace.split("::").each do |space| %>
+namespace <%= space %>{
+<% end %>
+
+    class <%= task.basename %>Deployer : public orogen::Deployer{
+        virtual ::RTT::TaskContext* getTask(const std::string &instance_name) const{
+            return new <%= task.name %>(instance_name);
+        };
+    };
+<% task.full_namespace.split("::").each do |space| %>
+}
+<% end %>
+
+CLASS_LOADER_REGISTER_CLASS(<%= task.full_namespace %>::<%= task.basename %>Deployer, ::orogen::Deployer);
+
+<% end %>
